5bd7356a5204a5583d2f32768896df7b014d21e2
HerreraGenerEmilio <herrerageneremilio@gmail.com>
2024-04-21 20:16:31 +0200

    commit para revertir

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth-service.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.credentials).subscribe(
+      response => {
+        localStorage.setItem('token', response.token);
+        // Redirigir o mostrar un mensaje de éxito
+      },
+      error => {
+        // Manejar el error
+        console.log(error);
+      }
+    );
+  }
 }
